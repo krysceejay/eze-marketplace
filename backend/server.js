0b4bc910402ad2aa1abafd598fa4578d605a6bd0
krysceejay <chrischimachijioke@gmail.com>
2021-05-24 14:13:03 +0100

    changed production config

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,6 @@ app.use(express.json())
 //Use routes
 app.use('/api/device', users)
 
-//app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
